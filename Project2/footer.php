6aa41493241739b7bc817f2fa084e79f16be38f2
rjs76 <35549025+rjs76@users.noreply.github.com>
2018-05-11 17:21:44 -0400

    Delete footer.php

--- Project2/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <a class="btn btn-primary mt-3" href="../todo/index.php?action=logout">Log Out</a>
-    <p class="copyright">
-        &copy; <?php echo date("Y"); ?> Alice W.
-    </p>
-</footer>
-</body>
-</html>
